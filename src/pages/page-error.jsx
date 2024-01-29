d1febecb9eb66e4386c00d71edb682155c65942c
user <user@192.168.1.2>
2024-01-29 17:06:56 +0500

    updated

--- src/pages/page-error.jsx
+++ src/pages/page-error.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function PageError() {
     return (
         <div className='error-section'>
             <h1>page doesn't exist</h1>
-            <button>return to home page</button>
+            <button>
+                <Link to={'/'}>return to home page</Link>
+            </button>
         </div>
     );
 };
